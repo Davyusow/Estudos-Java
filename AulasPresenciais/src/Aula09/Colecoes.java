bbf8274b5cba37fea7546b2a463fa3df380693cd
davyusow <dayvsonf27@gmail.com>
2025-06-30 20:59:50 -0300

    Conserto dos imports

---
+++ AulasPresenciais/src/Aula09/Colecoes.java
@@ -0,0 +1,14 @@
+package Aula09;
+
+import java.util.*;
+
+public class Colecoes {
+    public static void main(String[] args) {
+        Collection<String> coisas;
+        Integer num1 = 10;
+        Integer num2 = 11;
+        System.out.println(num1.compareTo(num2));
+        String nome ="asdasd";
+        nome.length();
+    }
+}
