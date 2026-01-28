8cdbf533d38feba369304e2f2f4a875e012b161a
Davyusow <dayvsonf27@gmail.com>
2026-01-27 23:22:15 -0300

    Início da revisão das novidades do Java 8

---
+++ revisao-2026/exemplosv8/Exemplo01.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Exemplo01 {
+
+    public static void main(String[] args) {
+        List<String> turma = Arrays.asList("Beltrano", "Ciclano", "Ademar");
+        turma.forEach(nome -> System.out.println(nome)); // Faz uma "ação" a cada iteração, no caso uma lambda
+        //que chama cada elemento da lista de nome, e imprime cada um
+        //Como é algo simples para precisar de uma função, e só foi usado aqui, uma lambda já basta
+
+        Runnable r = () -> System.out.println("Thread Iniciada!");
+        new Thread(r).start();
+    }
+}
---
+++ revisao-2026/exemplosv8/Exemplo02.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Exemplo02 {
+
+    public static void main(String[] args) {
+        List<String> nomes = Arrays.asList(
+            "Beltrano",
+            "Ciclano",
+            "Anderlaine",
+            "Frederico",
+            "Gabriel"
+        );
+
+        List<String> resultado = nomes
+            .stream()
+            .filter(nome -> nome.startsWith("A") || nome.startsWith("B")) //filtra
+            .sorted() //ordena
+            .collect(Collectors.toList()); //retorna uma lista
+
+        Integer totalCaracteres = nomes.stream().mapToInt(String::length).sum();
+
+        System.out.println("Nomes que começam com A: %s".formatted(resultado));
+
+        System.out.println(
+            "Total de caracteres de todos os nomes: %2d".formatted(totalCaracteres)
+        );
+
+        //Queremos apenas os que os nomes que comecem com 'A' ou 'B' Sejam retornados
+    }
+}
