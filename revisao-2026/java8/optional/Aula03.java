ef2d7981b44ea2e47b75d9badaf1be375d5dbb67
Davyusow <dayvsonf27@gmail.com>
2026-01-29 01:19:11 -0300

    Adição do arquivo dos estudos de optional

---
+++ revisao-2026/java8/optional/Aula03.java
@@ -0,0 +1,28 @@
+package java8.optional;
+
+import java.util.Optional;
+
+public class Aula03 {
+
+    public static Optional<Integer> converteEmNumero(String numeroStr) {
+        try {
+            Integer resultado = Integer.valueOf(numeroStr);
+            return Optional.of(resultado);
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "10";
+        var numero = converteEmNumero(s);
+        numero.ifPresent(System.out::println); // Só esxecuta, se tiver um valor dentro do optional
+        System.out.println(numero.orElse(0)); // Imprime o número, senão imprime um valor padrão
+        System.out.println(numero.orElseGet(() -> 0)); // O mesmo do else, mas com um lambda
+
+        System.out.println(
+            numero.orElseThrow(() -> new NullPointerException("Valor vazio!"))
+        );
+        // Se o valor não estiver, lança uma exceção!
+    }
+}
